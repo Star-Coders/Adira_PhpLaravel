0123102315e39be4fe89eaef160bf2b4bfcdc117
elvy <el.cayro@gmail.com>
2024-02-18 18:36:18 -0300

    § os carousels do catalogo foram trocados por cards para conseguir fazer a tela ainda hoje, domingo 18 fev

---
+++ resources/views/catalogo/cardGeral.blade.php
@@ -0,0 +1,51 @@
+
+@php
+
+// Definir as variáveis de ambiente
+$dbConnection = env('DB_CONNECTION');
+$dbHost = env('DB_HOST');
+$dbName = env('DB_DATABASE');
+$dbUsername = env('DB_USERNAME');
+$dbPassword = env('DB_PASSWORD');
+
+// Instanciar o objeto PDO
+$pdo = new PDO(
+    "$dbConnection:host=$dbHost;dbname=$dbName",
+    $dbUsername,
+    $dbPassword
+);
+
+// Busca no database
+$statement = $pdo->prepare('SELECT id,fotografia,categoria,descricao FROM itens ORDER BY RAND() LIMIT 77');
+$statement->execute();
+
+
+  $carousel = []; $i=0;
+
+  while($cardinfo = $statement->fetchAll(PDO::FETCH_ASSOC)){
+    
+    $carousel[$i] = $cardinfo;
+    $i++;
+  }
+@endphp
+
+
+<div class="row">
+    @for($i=0; $i<sizeof($carousel); $i++)
+
+      @foreach($carousel[$i] as $card)
+
+          <div class="card">
+            {{-- <img src="@php echo $card['fotografia'] @endphp" class="card-img-top" alt="Produto para alugar."> --}}
+            <div class="card-body">
+              <h5 class="card-title">@php echo $card['categoria']; @endphp</h5>
+              <p class="card-text">@php echo $card['descricao']; @endphp</p>
+              <div class="card-footer">
+
+                <a href="{{route('produtos.solicitar-produto', $card['id'])}}" class="btn btn-primary"><strong>QUERO ALUGAR</strong></a>
+              </div>
+            </div>
+          </div>
+        @endforeach
+    @endfor
+</div>
--- resources/views/catalogo/home.blade.php
+++ resources/views/catalogo/home.blade.php
@@ -7,23 +7,54 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <style>
+        .card{
+            display: flex;
+            justify-content:space-between;
+            margin: 2vh;
+            max-width: 39vh;
+        }
+        .card-text{
+            margin-bottom: 52px;
+        }
+
+        .card-footer {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        }
+
+    </style>
 </head>
 <body class="bg-warning bg-gradient">
     @include('layouts.admin.nav')
     <main>
 
         <div class="container p-4">
-            @include('catalogo.carouselGeral')
+            <h2>Catálogo Geral das Opções Disponíveis para Aluguel</h2>
+            @include('catalogo.cardGeral')
         </div>
-
-        <div class="container">
-            @include('catalogo.carouselAvaliados')
+        <div class="container p-4">
+            <h2>Produtos Mais Bem-Avaliados</h2>
+            @include('catalogo.cardAvaliados')
         </div>
-
-        <div class="container">
-            @include('catalogo.carouselMaisAlugados')
+        <div class="container p-4">
+            <h2>Produtos Mais Alugados</h2>
+            @include('catalogo.cardMaisAlugados')
         </div>
     </main>
-
+    
 </body>
 </html>
+{{-- <div class="container p-4">
+    @include('catalogo.carouselGeral')
+</div>
+
+<div class="container">
+    @include('catalogo.carouselAvaliados')
+</div>
+
+<div class="container">
+    @include('catalogo.carouselMaisAlugados')
+</div> --}}
