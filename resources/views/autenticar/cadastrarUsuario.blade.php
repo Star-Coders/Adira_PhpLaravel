7e727c4c5f43043fdba6a92385c6b11017412923
Bruno Marcos Keil <brunomarcoskeil@gmail.com>
2024-02-18 22:22:37 -0300

    modificação na tela cadastrarUsuario, para que os campos fiquem divididos em colunas

--- resources/views/autenticar/cadastrarUsuario.blade.php
+++ resources/views/autenticar/cadastrarUsuario.blade.php
@@ -11,51 +11,61 @@
 <body class="bg-warning bg-gradient">
     <main>
         <div class="container vh-100">
-            <div class="row justify-content-center align-items-center h-75">
+            <div class="row justify-content-center align-items-center h-100">
                 <div class="col col-sm-12 col-md-6">
                     <div class="card">
                         <div class="container mt-5">
                             <h2>Cadastro de Usuário</h2>
                             <form>
-                                <div class="mb-3">
-                                    <label for="nome" class="form-label">Nome</label>
-                                    <input type="text" class="form-control" id="nome" name="nome" required>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="nome" class="form-label">Nome</label>
+                                        <input type="text" class="form-control" id="nome" name="nome" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="email" class="form-label">Email</label>
+                                        <input type="email" class="form-control" id="email" name="email" required>
+                                    </div>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" required>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="senha" class="form-label">Senha</label>
+                                        <input type="password" class="form-control" id="senha" name="senha" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="telefone" class="form-label">Telefone</label>
+                                        <input type="text" class="form-control" id="telefone" name="telefone" required>
+                                    </div>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="senha" class="form-label">Senha</label>
-                                    <input type="password" class="form-control" id="senha" name="senha" required>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="cep" class="form-label">CEP</label>
+                                        <input type="text" class="form-control" id="cep" name="cep" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="uf" class="form-label">UF</label>
+                                        <input type="text" class="form-control" id="uf" name="uf" maxlength="2" required>
+                                    </div>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="telefone" class="form-label">Telefone</label>
-                                    <input type="text" class="form-control" id="telefone" name="telefone" required>
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="cidade" class="form-label">Cidade</label>
+                                        <input type="text" class="form-control" id="cidade" name="cidade" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="logradouro" class="form-label">Logradouro</label>
+                                        <input type="text" class="form-control" id="logradouro" name="logradouro" required>
+                                    </div>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="cep" class="form-label">CEP</label>
-                                    <input type="text" class="form-control" id="cep" name="cep" required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="uf" class="form-label">UF</label>
-                                    <input type="text" class="form-control" id="uf" name="uf" maxlength="2" required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="cidade" class="form-label">Cidade</label>
-                                    <input type="text" class="form-control" id="cidade" name="cidade" required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="logradouro" class="form-label">Logradouro</label>
-                                    <input type="text" class="form-control" id="logradouro" name="logradouro" required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="numero" class="form-label">Número</label>
-                                    <input type="number" class="form-control" id="numero" name="numero" required>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="complemento" class="form-label">Complemento</label>
-                                    <input type="text" class="form-control" id="complemento" name="complemento">
+                                <div class="row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="numero" class="form-label">Número</label>
+                                        <input type="number" class="form-control" id="numero" name="numero" required>
+                                    </div>
+                                    <div class="col-md-6 mb-3">
+                                        <label for="complemento" class="form-label">Complemento</label>
+                                        <input type="text" class="form-control" id="complemento" name="complemento">
+                                    </div>
                                 </div>
                                 <button type="submit" class="btn btn-primary">Cadastrar</button>
                             </form>
