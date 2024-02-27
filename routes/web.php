08207d7c97409d4b6300de1a45daf3a1e715c538
Lucas Augusto Gorges <82848715+LucasAugustog@users.noreply.github.com>
2024-02-26 22:21:31 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductReturnController;
+
+Route::get('/return-product', [ProductReturnController::class, 'showReturnForm'])->name('return-product');
+Route::post('/return-product', [ProductReturnController::class, 'processReturn']);
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -20,3 +26,12 @@ Route::get('/', function () {
 Route::get('/devolucao', function () {
     return view('devolucao');
 });
+
+
+
+
+Route::get('/adicionar-produto', [ProdutoController::class, 'create']);
+Route::post('/salvar-produto', [ProdutoController::class, 'store'])->name('salvar.produto');
+
+
+
